752e12d768f0d2f6b4be991258f010196f44ef0c
Alex Kessinger <voidfiles@gmail.com>
2020-02-17 20:16:46 -0700

    Running a thing

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context" // Use "golang.org/x/net/context" for Golang version <= 1.6
 	"flag"
+	"log"
+	"net"
 	"net/http"
+	"sync"
 
 	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
@@ -18,22 +21,50 @@ var (
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
 )
 
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	gw.UnimplementedMagdaServiceServer
+}
+
 func run() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	lis, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	gw.RegisterMagdaServiceServer(s, &server{})
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := gw.RegisterMagdaServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err = gw.RegisterMagdaServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
 	if err != nil {
 		return err
 	}
 
+	var wg = sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(":8081", mux)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		http.ListenAndServe(":8081", mux)
+	}()
+
+	wg.Wait()
+
+	return nil
 }
 
 func main() {
